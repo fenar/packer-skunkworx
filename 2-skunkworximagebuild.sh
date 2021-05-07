90231ebb05c45ac6e219c87f6370de9a5f48cf70
Fatih Nar <fenar@yahoo.com>
2021-05-07 17:46:06 -0500

    Update 2-skunkworximagebuild.sh

--- 2-skunkworximagebuild.sh
+++ 2-skunkworximagebuild.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #Author: fenar
 tar xvf -C iso/coreos.tar.gz skunkworx
-current-dir=pwd
+current_dir=$PWD
 cd skunkworx
 sudo mkdir boot/iso
 sudo cp iso/discovery_image.iso boot/iso/
 sudo cp config/40_custom etc/grub.d/
-cd $current-dir
+cd $current_dir
 sudo tar -czvf skunkworx-image.tar.gz skunkworx

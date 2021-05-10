d0354d0b7e275785f6baa422e61ba19cb95f0743
Fatih Nar <fenar@yahoo.com>
2021-05-10 09:15:11 -0500

    Update 3-uploadimage.sh

--- 3-uploadimage.sh
+++ 3-uploadimage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Author: fenar
-maas admin boot-resources create name='centos/ai-disco' title='CoreOS' architecture='amd64/generic' filetype='tgz' content@=iso/coreos.tar.gz
+maas admin boot-resources create name='centos/ai-disco' title='CoreOS' architecture='amd64/generic' filetype='tgz' content@=iso/skunkworx-image.tar.gz
